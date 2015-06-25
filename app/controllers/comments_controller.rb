7b2ce7cc281801d7cbb303dddf13518788927ffa
TouanAin <touanain@gmail.com>
2015-06-25 16:29:25 +0700

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,6 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:content,:question_id,:user_id)
+      params.require(:comment).permit(:content,:entry_id,:user_id)
     end
 end
